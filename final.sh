987b0d018e136f77f49eb168b9cdb32c4a08822a
admin <garten323@gmail.com>
2020-03-16 14:32:53 -0500

    Fixing logs

--- final.sh
+++ final.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # This direcotry is hardcoded and can be changed. This monitors changes to the request direcotry.
 # When a change is made, then it executes junk in the middle.
 DIR=/home/pi/Mark
@@ -10,7 +10,8 @@ while inotifywait -qqre modify "$dir1"; do
 export GOOGLE_APPLICATION_CREDENTIALS="auth/credentials.json"
 #
 # This requests and creates a base 64 encoded mp3 file that conatins what was requested.
-/usr/bin/curl -s -X POST -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) -H "Content-Type: application/json; charset=utf-8" -d @request/request.json  https://texttospeech.googleapis.com/v1/text:synthesize > tmp/synthesize.txt
+echo "Encoded mp3" > $LOG
+/usr/bin/curl -s -X POST -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) -H "Content-Type: application/json; charset=utf-8" -d @request/request.json  https://texttospeech.googleapis.com/v1/text:synthesize > tmp/synthesize.txt > $LOG
 #
 # This removes all spaces and junk and synthesizes the text file.
 /bin/sed -i 's/\(}\| \|{\|audioContent\|"\|:\)//g' tmp/synthesize.txt
