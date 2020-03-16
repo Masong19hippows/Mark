43187e9dc8ebeb49ae9307d6c4e2dc83cf2f2cc6
admin <garten323@gmail.com>
2020-03-16 15:39:37 -0500

    Making everything work on Autotart

--- edit.sh
+++ edit.sh
@@ -1,1 +1,2 @@
+DIR=/home/pi/Mark
 sed -i 's|"text":".*"|"text":"'"$1"'"|g' request/request.json
--- final.sh
+++ final.sh
@@ -2,23 +2,23 @@
 # This direcotry is hardcoded and can be changed. This monitors changes to the request direcotry.
 # When a change is made, then it executes junk in the middle.
 DIR=/home/pi/Mark
-LOG=tmp/log.txt
+LOG=/home/pi/Mark/tmp/log.txt
 dir1=/home/pi/Mark/request/
 while inotifywait -qqre modify "$dir1"; do
 #
 # This sets the credentials to the Google TTS API credentials file.
 DIR=/home/pi/Mark
-export GOOGLE_APPLICATION_CREDENTIALS="auth/credentials.json"
+export GOOGLE_APPLICATION_CREDENTIALS="/home/pi/Mark/auth/credentials.json"
 #
 # This requests and creates a base 64 encoded mp3 file that conatins what was requested.
 echo "Encoded mp3" > $LOG
-/usr/bin/curl -s -X POST -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) -H "Content-Type: application/json; charset=utf-8" -d @request/request.json  https://texttospeech.googleapis.com/v1/text:synthesize > tmp/synthesize.txt
+/usr/bin/curl -s -X POST -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) -H "Content-Type: application/json; charset=utf-8" -d @/home/pi/Mark/request/request.json  https://texttospeech.googleapis.com/v1/text:synthesize > /home/pi/Mark/tmp/synthesize.txt
 #
 # This removes all spaces and junk and synthesizes the text file.
-/bin/sed -i 's/\(}\| \|{\|audioContent\|"\|:\)//g' tmp/synthesize.txt
+/bin/sed -i 's/\(}\| \|{\|audioContent\|"\|:\)//g' /home/pi/Mark/tmp/synthesize.txt
 #
 # This decodes then autoplays the synthesized mp3 file.
-/usr/bin/base64 tmp/synthesize.txt --decode > final/synthesized.mp3
-/usr/bin/mplayer -novideo -idle -slave final/synthesized.mp3 </dev/null >/dev/null 2>&1 &
+/usr/bin/base64 /home/pi/Mark/tmp/synthesize.txt --decode > /home/pi/Mark/final/synthesized.mp3
+/usr/bin/mplayer -novideo -idle -slave /home/pi/Mark/final/synthesized.mp3 </dev/null >/dev/null 2>&1 &
 #
 done
