4d1ac27e3e74a0721abb3671be3625bce38989c9
admin <garten323@gmail.com>
2020-03-17 09:58:00 -0500

    Mark

--- Mark.sh
+++ Mark.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+#
+# This lets the system startup and changes the directory to the working directory.
 sleep 10
+cd /home/pi/Mark
+#
+# This sets the script that monitors the request folder. Then says a gerneral greeting.
+./final.sh &
+sleep 1
+./edit.sh 'Welcome Mason, My name is Mark.'
