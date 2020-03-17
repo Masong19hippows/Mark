7f9ff9f7334b6f5f9cfafe4766ecf91975028e46
admin <garten323@gmail.com>
2020-03-17 09:27:57 -0500

    Autostart

--- Mark.sh
+++ Mark.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+#
+# Sleeps for 10 seconds to wait for system to boot then executes final.sh.
 sleep 10
+# Changes the directort to working directory and sets a cool thing.
+cd /home/pi/Mark
+./final.sh &
+./edit.sh 'Welcome Mason, My name is Mark.'
--- final.sh
+++ final.sh
@@ -10,7 +10,7 @@ while inotifywait -qqre modify "$dir1"; do
 export GOOGLE_APPLICATION_CREDENTIALS="auth/credentials.json"
 #
 # This requests and creates a base 64 encoded mp3 file that conatins what was requested.
-echo "Encoded mp3" > $LOG
+echo "Encoded mp3" >> $LOG
 /usr/bin/curl -s -X POST -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) -H "Content-Type: application/json; charset=utf-8" -d @request/request.json  https://texttospeech.googleapis.com/v1/text:synthesize > tmp/synthesize.txt
 #
 # This removes all spaces and junk and synthesizes the text file.
@@ -18,6 +18,6 @@ echo "Encoded mp3" > $LOG
 #
 # This decodes then autoplays the synthesized mp3 file.
 /usr/bin/base64 tmp/synthesize.txt --decode > final/synthesized.mp3
-/usr/bin/mplayer -novideo -idle -slave final/synthesized.mp3 </dev/null >/dev/null 2>&1 &
+# /usr/bin/mplayer -novideo -idle -slave final/synthesized.mp3 </dev/null >/dev/null 2>&1 &
 #
 done
