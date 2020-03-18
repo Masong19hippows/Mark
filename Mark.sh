2afe9d65d912b35acd099201c9caf4bb4593d531
admin <garten323@gmail.com>
2020-03-18 09:16:09 -0500

    Better

--- Mark.sh
+++ Mark.sh
@@ -5,8 +5,8 @@ sleep 10
 cd /home/pi/Mark
 #
 # This sets the script that monitors the request folder. Then says a gerneral greeting.
-./notify.sh &
+# ./notify.sh &
 sleep 1
 ./edit.sh "Welcome Mason, my name is Mark."
 #
-echo $name
+echo $uservar
